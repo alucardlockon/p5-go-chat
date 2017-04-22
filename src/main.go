a30c920bf3dc9ea42c4d278fb03a90be85f7a43d
alucardlockon <alucardlockon3@gmail.com>
2017-04-23 00:27:24 +0800

    2017-04-23 00:27:22

---
+++ src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"log"
+	"html/template"
+	"database/sql"
+	_ "github.com/Go-SQL-Driver/MySQL"
+)
+
+func index(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path",r.URL.Path)
+	fmt.Println("scheme",r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k,v:=range r.Form{
+		fmt.Println("key:",k)
+		fmt.Println("val:",strings.Join(v,""))
+	}
+	//fmt.Fprint(w,"Hello p5")
+	t,_:=template.ParseFiles("web/main.html")
+	t.Execute(w,nil)
+	sql.Open("mysql","root:1234@/test?charset=utf8")
+}
+
+func main(){
+	http.HandleFunc("/",index)
+	err:=http.ListenAndServe(":9090",nil)
+	if err!=nil{
+		log.Fatal("ListenAndServe:",err)
+	}
+}
